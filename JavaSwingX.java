a27e7ce4d416b67fab90f0874b2b7e4747991756
Matheus 1°DS AMS <theusmoreira00@gmail.com>
2025-06-11 15:07:18 -0300

    Add files via upload

---
+++ JavaSwingX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaswingx;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class JavaSwingX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
